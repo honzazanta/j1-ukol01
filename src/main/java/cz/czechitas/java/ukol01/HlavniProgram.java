af7df3a9462e9f857ea33aabd83fac0fc19b5103
Jan Žanta <honza.zanta@email.cz>
2023-02-18 20:39:13 +0100

    prasátko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -5,9 +5,49 @@ import cz.czechitas.java.ukol01.engine.*;
 public class HlavniProgram {
 
     public void start() {
+        //pohyb Žofky do do pravé strany plátna
         Turtle zofka = new Turtle();
 
-        //TODO implementace domácího úkolu
+        //nakreslení těla a hlavy prásatka
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.turnLeft(45);
+        zofka.move(70);
+        zofka.turnLeft(90);
+        zofka.move(70);
+        zofka.turnLeft(135);
+        zofka.move(100);
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnLeft(90);
+
+        // nohy prasátka
+        zofka.move(150);
+        zofka.turnRight(45);
+        zofka.move(40);
+        zofka.turnRight(180);
+        zofka.move(40);
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnLeft(180);
+        zofka.move(40);
+        zofka.turnLeft(135);
+        zofka.move(150);
+        zofka.turnLeft(135);
+        zofka.move(40);
+        zofka.turnLeft(180);
+        zofka.move(40);
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnLeft(180);
+        zofka.move(40);
+
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(400);
+        zofka.turnLeft(90);
+        zofka.penDown();
     }
 
     public static void main(String[] args) {
