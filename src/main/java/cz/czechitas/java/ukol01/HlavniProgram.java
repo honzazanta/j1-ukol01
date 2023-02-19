a658658f336428550de541be4ee632714d0a7714
Jan Žanta <honza.zanta@email.cz>
2023-02-19 21:53:40 +0100

    úkol01 - část 4 hotovo

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -67,6 +67,37 @@ public class HlavniProgram {
         zofka.move(400);
 
         this.drawSun();
+        zofka.turnLeft(135);
+        zofka.move(650);
+        zofka.turnRight(180);
+        zofka.penDown();
+
+        this.nakresliH();
+        zofka.penUp();
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        this.drawCircle();
+        zofka.penUp();
+        zofka.move(80);
+        zofka.turnLeft(90);
+        zofka.penDown();
+        this.nakresliN();
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(50);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.penDown();
+        this.nakresliZ();
+        zofka.penUp();
+        zofka.move(50);
+        zofka.penDown();
+        zofka.turnLeft(90);
+        this.nakresliA();
+
 
         //zofka.turnRight(180);
         //zofka.move(150);
@@ -150,6 +181,46 @@ public class HlavniProgram {
             zofka.move(45);
         }
         zofka.penUp();
+    }
+    public void nakresliH() {
+        zofka.move(100);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnLeft(90);
+        zofka.move(65);
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnRight(180);
+    }
+
+    public void nakresliN() {
+        zofka.move(100);
+        zofka.turnRight(135);
+        zofka.move(140);
+        zofka.turnLeft(135);
+        zofka.move(100);
+    }
+
+    public void nakresliZ() {
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(135);
+        zofka.move(150);
+        zofka.turnLeft(135);
+        zofka.move(100);
+    }
+
+    public void nakresliA() {
+        zofka.turnRight(30);
+        zofka.move(125);
+        zofka.turnRight(120);
+        zofka.move(125);
+        zofka.turnRight(180);
+        zofka.move(60);
+        zofka.turnLeft(60);
+        zofka.move(60);
     }
 
     public static void main(String[] args) {
