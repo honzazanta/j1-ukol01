81db318bfc9378e949c5ed4b1985e875d29df494
Jan Žanta <honza.zanta@email.cz>
2023-02-19 14:37:16 +0100

    úkol01 - část 2 hotovo

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -1,45 +1,36 @@
- package cz.czechitas.java.ukol01;
+package cz.czechitas.java.ukol01;
 
 import cz.czechitas.java.ukol01.engine.*;
 
 public class HlavniProgram {
 
-    public void nakresliPrasatko() {
-        //nakreslení těla a hlavy prásatka
-        Turtle zofka = new Turtle();
-        zofka.move(100);
-        zofka.turnLeft(90);
-        zofka.move(120);
-        zofka.turnLeft(45);
-        zofka.move(70);
-        zofka.turnLeft(90);
-        zofka.move(70);
-        zofka.turnLeft(135);
-        zofka.move(100);
-        zofka.turnLeft(180);
-        zofka.move(100);
-        zofka.turnLeft(90);
-        zofka.move(120);
-    }
+    private Turtle zofka = new Turtle();
 
     public void start() {
-        Turtle zofka = new Turtle();
-        zofka.move(100);
-        zofka.turnLeft(90);
-        zofka.move(120);
-        zofka.turnLeft(45);
-        zofka.move(70);
-        zofka.turnLeft(90);
-        zofka.move(70);
-        zofka.turnLeft(135);
-        zofka.move(100);
-        zofka.turnLeft(180);
-        zofka.move(100);
-        zofka.turnLeft(90);
-        zofka.move(120);
+        //this.nakresliCtverec();
+        //this.nakresliNohy();
+        //this.nakresliTrojúhelník();
+        //zofka.turnRight(180);
+        //zofka.move(150);
+        //this.drawCircle();
+        //this.drawSun();
+        //this.drawOctagon();
+    }
 
-        //první noha prasátka
-        zofka.turnRight(45);
+    private void nakresliCtverec() {
+        //nakreslení těla a hlavy prásatka
+        zofka.penDown();
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnLeft(90);
+        }
+        zofka.penUp();
+    }
+
+    public void nakresliNohy() {
+        zofka.penDown();
+        zofka.turnLeft(180);
+        zofka.turnLeft(45);
         zofka.move(40);
         zofka.turnLeft(180);
         zofka.move(40);
@@ -50,7 +41,7 @@ public class HlavniProgram {
 
         //přesunutí mezi nohami
         zofka.turnLeft(135);
-        zofka.move(120);
+        zofka.move(100);
 
         zofka.turnLeft(135);
         zofka.move(40);
@@ -60,11 +51,54 @@ public class HlavniProgram {
         zofka.move(40);
         zofka.turnLeft(180);
         zofka.move(40);
+        zofka.penUp();
+    }
 
+    public void nakresliTrojúhelník() {
+        zofka.penDown();
+        zofka.turnLeft(90);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(70);
+        zofka.turnRight(135);
+        zofka.move(70);
+        zofka.penUp();
+    }
+
+    public void drawCircle() {
+        zofka.penDown();
+        for (int j = 0; j < 360; j++) {
+                zofka.turnLeft(1);
+                zofka.move(1);
+            }
+        zofka.penUp();
+    }
+
+    public void drawSun() {
+        zofka.penDown();
+        for (int j = 0; j < 18; j++) {
+            zofka.turnLeft(20);
+            zofka.move(20);
+            zofka.turnRight(90);
+            zofka.move(20);
+            zofka.turnRight(180);
+            zofka.move(20);
+            zofka.turnRight(90);
+        }
+        zofka.penUp();
+    }
+    public void drawOctagon() {
+        zofka.penDown();
+        for (int j = 0; j < 8; j++) {
+            zofka.turnLeft(45);
+            zofka.move(45);
+        }
+        zofka.penUp();
     }
 
     public static void main(String[] args) {
         new HlavniProgram().start();
     }
-
 }
+
+
